17c3c8920edb2b56d7b4ac5ce4b22e35aa6ec232
Vasken Hauri <vhauri@gmail.com>
2011-09-22 13:15:43 -0400

    initial socialTracker commit

---
+++ jquery.socialtracker.js
@@ -0,0 +1,80 @@
+/*
+ * jQuery Social Tracker Plugin
+ * @author Vasken Hauri
+ * Enables easy implementation of Google analytics social tracking
+ * a la http://code.google.com/apis/analytics/docs/tracking/gaTrackingSocial.html
+ */
+(function( $ ){
+	function extractParamFromUri(uri, paramName) {
+		if (!uri) {
+			return;
+		}
+		var uri = uri.split('#')[0];  // Remove anchor.
+		var parts = uri.split('?');  // Check for query params.
+		if (parts.length == 1) {
+			return;
+		}
+		var query = decodeURI(parts[1]);
+
+		// Find url param.
+		paramName += '=';
+		var params = query.split('&');
+		for (var i = 0, param; param = params[i]; ++i) {
+			if (param.indexOf(paramName) === 0) {
+				return unescape(param.split('=')[1]);
+			}
+		}
+	}
+
+	var methods = {
+		init : function(options) { 
+						var settings = {
+							twitter: true,
+							facebook: true,
+						}
+
+						 options = $.extend(options, settings);
+						 
+						 try {
+							 if(FB && FB.Event && FB.Event.subscribe) {
+								 FB.Event.subscribe('edge.create', function(targetUrl) {
+									 _gaq.push(['_trackSocial', 'facebook', 'like', targetUrl]);
+								 });
+								 FB.Event.subscribe('message.send', function(targetUrl) {
+									 _gaq.push(['_trackSocial', 'facebook', 'send', targetUrl]);
+								 });
+							 }
+						 } catch(e) {}
+
+						 try {
+							 if(twttr && twttr.events) {
+								 twttr.events.bind('tweet', function(event) {
+									 if (event) {
+										 var targetUrl;
+										 if (event.target && event.target.nodeName == 'IFRAME') {
+											 targetUrl = extractParamFromUri(event.target.src, 'url');
+										 }
+										 _gaq.push(['_trackSocial', 'twitter', 'tweet', targetUrl]);
+									 }
+								 });
+							 }
+						 } catch(e) {}
+					 
+						return 'Social tracking enabled';
+					 }
+	};
+
+	$.fn.socialTracker = function( method ) {
+
+		// Method calling logic
+		if ( methods[method] ) {
+			return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		} else if ( typeof method === 'object' || ! method ) {
+			return methods.init.apply( this, arguments );
+		} else {
+			$.error( 'Method ' +  method + ' does not exist on jQuery.socialTracker' );
+		}    
+
+	};
+
+})( jQuery );
